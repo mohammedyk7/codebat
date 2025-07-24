600d841db6df28896bec49c9adbdeb10f4ad1d21
unknown <mohammedyk7@gmail.com>
2025-07-24 11:28:55 +0400

    hi

---
+++ warmup/Monkey.java
@@ -0,0 +1,18 @@
+package warmup;
+
+public class Monkey {
+    public String monkeyTrouble(boolean aSmile, boolean bSmile) {
+        // Return "true" if both monkeys are smiling or both are not smiling
+        return (aSmile == bSmile) ? "true" : "false";
+    }
+
+    public static void main(String[] args) {
+        Monkey obj = new Monkey();
+
+        // Test cases
+        System.out.println(obj.monkeyTrouble(true, true));   // should print "true"
+        System.out.println(obj.monkeyTrouble(false, false)); // should print "true"
+        System.out.println(obj.monkeyTrouble(true, false));  // should print "false"
+        System.out.println(obj.monkeyTrouble(false, true));  // should print "false"
+    }
+}
---
+++ warmup/Sleeping.java
@@ -0,0 +1,13 @@
+package warmup;
+
+public class Sleeping {
+    public boolean sleepIn(boolean weekday, boolean vacation) {
+        return !weekday || vacation;
+    }
+
+    public static void main(String[] args) {
+        Sleeping obj = new Sleeping();
+        System.out.println(obj.sleepIn(false, false)); // should print true
+        System.out.println(obj.sleepIn(true, false));  // should print false
+    }
+}
